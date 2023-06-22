949958dfbf40c8e2be9996522e7459c30670d02f
Guilherme S. Blanco <guiseco@gmail.com>
2023-06-21 21:05:17 -0300

    Corrige o BUG que não mostra as acessbilidades na tela ADM/Estabelecimentos.
    Corrige o BUG que não mostra as acessbilidades na tela ADM/Estabelecimentos.

--- src/app/pages/admin/admin-places/admin-places.component.ts
+++ src/app/pages/admin/admin-places/admin-places.component.ts
@@ -124,7 +124,7 @@ export class AdminPlacesComponent implements OnInit {
   }
 
   checkIfAccessibiltyExists(place: any, accessibilityName: string) {
-    const accessibilty = place.accessibilities.filter(e => e.Name === accessibilityName);
+    const accessibilty = place.accessibilities.filter(e => e.name === accessibilityName);
     return accessibilty.length > 0 && accessibilty[0].has;
   }
 
